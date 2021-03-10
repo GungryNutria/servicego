0e796b96433289683492be244ecf32a3d02b4147
GungryNutria <edgardochamorro08@gmail.com>
2021-03-10 09:49:35 -0600

    10/03/2021

---
+++ lib/pages/detail_service_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DetailServicePage extends StatefulWidget {
+  DetailServicePage({Key key}) : super(key: key);
+
+  @override
+  _DetailServicePageState createState() => _DetailServicePageState();
+}
+
+class _DetailServicePageState extends State<DetailServicePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(child: Text('Producto')),
+    );
+  }
+}
